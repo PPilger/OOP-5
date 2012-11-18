b44625802ee1c8902fd215f418800bb4193d0e13
kletzi <kletzi@192.168.0.11>
2012-11-18 23:47:27 +0100

    Zusicherungen v1

--- src/Description.java
+++ src/Description.java
@@ -1,14 +1,23 @@
-
+/**
+ * Untertyp von Shorter
+ * 
+ * @author Christian Kletzaner
+ *
+ */
 public class Description implements Shorter<Description> {
 	
 	private String text;
 	
+	//Defniert einen Text
 	public Description(String text) {
 		this.text = text;
 	}
 	
 	/**
 	 * NOTE: Vergleicht die Laengen der Texte.
+	 * 		 Wenn der uebergebene Text kleiner ist als
+	 * 		 der Text vom derzeitigen Objekt, dann wird
+	 * 		 TRUE zurueckgegeben.
 	 * 
 	 * @Override
 	 */
@@ -16,10 +25,12 @@ public class Description implements Shorter<Description> {
 		return this.text.length() < other.text.length();
 	}
 
+	//Gibt den Text des Objekts zurueck
 	public String toString() {
 		return this.text;
 	}
 	
+	//Gibt die Anzahl der Zeilen zurueck
 	public int lineCount() {
 		return this.text.split("\n").length;
 	}
--- src/Shorter.java
+++ src/Shorter.java
@@ -1,4 +1,13 @@
-
+/**
+ * NOTE: Die Methode shorter beschreibt nur, ob ein Objekt P kleiner ist
+ * 		 als das derzeitige Objekt und gibt ein TRUE zurueck, sobald das Vergleichsobjekt
+ * 		 kleiner ist als das derzeitige.
+ * 
+ * @author Christian Kletzander
+ *
+ * @param <P>
+ * 		Beschreibt ein beliebiges Objekt - generischer Typ
+ */
 public interface Shorter<P> {
 	public boolean shorter(P other);
 }
