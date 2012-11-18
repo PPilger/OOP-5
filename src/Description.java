1f85e4338adbe7797e46c9c32166ae77aa09a7f6
kletzi <kletzi@192.168.0.11>
2012-11-18 23:16:20 +0100

    Description

---
+++ src/Description.java
@@ -0,0 +1,28 @@
+
+public class Description implements Shorter<Description> {
+	
+	private String text;
+	
+	public Description(String text) {
+		this.text = text;
+	}
+	
+	/**
+	 * NOTE: Vergleicht die LŠngen der Text.
+	 * 
+	 * @Override
+	 */
+	public boolean shorter(Description other) {
+		return this.text.length() < other.text.length();
+	}
+
+	public String toString() {
+		return this.text;
+	}
+	
+	public int lineCount() {
+		return this.text.split("\n").length;
+	}
+	
+	
+}
