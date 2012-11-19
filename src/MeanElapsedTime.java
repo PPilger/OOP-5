8a8420853e7695ed21b780849cd15372d1a78261
koegleal-tu <koegleal_foren@hotmail.com>
2012-11-20 00:31:28 +0100

    kommentiert und zusicherungen gemacht

--- src/MeanElapsedTime.java
+++ src/MeanElapsedTime.java
@@ -1,10 +1,18 @@
 import java.util.Iterator;
-
+/**
+ * Stellt die Moeglichkeit zur Verfuegung Messwerte mit doppelter Genauigkeit abzuspeichern.  
+ * @author Koegler Alexander
+ *
+ */
 public class MeanElapsedTime extends ElapsedTime {
 
 	Set<Double> meassurement = new Set<Double>();
 
 	// @Override
+	/**
+	 * Zaehlt die enthaltenen Messwerte
+	 * Nachbedingung: gibt Zahl groeszer gleich 0 zurueck
+	 */
 	public int count() {
 		Iterator<Double> iti = meassurement.iterator();
 		int cnt = 0;
@@ -15,10 +23,19 @@ public class MeanElapsedTime extends ElapsedTime {
 		return cnt;
 	}
 
+	/**
+	 * Fuegt ein Objekt zu den Messwerten hinzu, sofern es nicht bereits eingefuegt wurde.
+	 * @param meassurement Das hinzuzufuegende Objekt
+	 */
 	public void addMeassurement(double meassurement) {
 		this.meassurement.insert(meassurement);
 	}
 
+	/**
+	 * Gibt den hoechsten Messwert aus
+	 * @return Peak
+	 * Nachbedingung: gibt Zahl groeszer 0 zurueck, gibt 0 zurueck falls keine Messwerte vorhanden sind.
+	 */
 	public double maxMeassurement() {
 		Iterator<Double> iti = meassurement.iterator();
 		double ret = 0;
@@ -33,6 +50,10 @@ public class MeanElapsedTime extends ElapsedTime {
 		return ret;
 	}
 
+	/**
+	 * DIe Zeit ist fuer diesen Typ der Durchschnittswert aller gespeicherten Messwerte
+	 * Nachbedingung: gibt Zahl groeszer gleich 0 zurueck
+	 */
 	protected double getTime() {
 		double ret = 0;
 		for (Double db : meassurement) {
